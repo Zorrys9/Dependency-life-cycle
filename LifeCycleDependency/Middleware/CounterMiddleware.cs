51b45826b373f0647ab39e2f63941e5d5f9bf2a6
Zorrys9 <saschek2000@yandex.ru>
2020-01-23 14:58:55 +0300

    Dependency life cycle

---
+++ LifeCycleDependency/Middleware/CounterMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LifeCycleDependency.Services;
+namespace LifeCycleDependency.Middleware
+{
+    public class CounterMiddleware
+    {
+        private RequestDelegate _next;
+        int i = 0;
+        public CounterMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context, ICounter counter, CounterService counterService)
+        {
+            i++;
+            context.Response.ContentType = "text/html;charset=utf-8";
+            await context.Response.WriteAsync($"Запрос {i}: ICounter = {counter.Value}; CounterService = {counterService.Counter.Value}");
+        }
+
+    }
+}
